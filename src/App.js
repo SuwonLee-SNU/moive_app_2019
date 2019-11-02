5bfe49f6d6281fe568fe28d9f49e0f27d7e673a6
Suwon <lsw7169@snu.ac.kr>
2019-11-02 16:20:16 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Food( {name,picture,rating} ) {
-  return (
-  <div>
-    <h6>I love {name}!!!!!!!</h6>
-    <h4> {rating}/5.0 </h4>
-    <img src={picture}/>
-  </div>
-  );
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () =>{
+    console.log("minus");
+  };
+  render() {
+    return (
+    <div>
+      <h1> The number is : {this.state.count} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    )
+  };
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image: "https://images.unsplash.com/photo-1487769723072-0e6602799af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-    rating: 3
-  },
-  {
-    id:2,
-    name: "Samgiopsal",
-    image: "https://images.unsplash.com/photo-1559602580-78f1ba809b92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    rating: 5
-  }
-];
-
-function App() {
-    return <div>
-      {foodILike.map(dish=>(
-      <Food key={dish.id}
-        name={dish.name} 
-        picture={dish.image}
-        rating={dish.rating}
-      />
-      ))}
-    </div>;
-}
-
 export default App;
